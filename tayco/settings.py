438acbe00f0e96654c8e5c2d65b9ec9ffe2b270b
Matambo <88248180+Arnyme@users.noreply.github.com>
2024-04-14 02:01:50 +0300

    Update settings.py
    modified SECURITY WARNINGS on debug options to False, and also included STATIC ROOT for reading static files

--- tayco/settings.py
+++ tayco/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-juxjupgae-7b^fl9)4et+1588zbh7usv0by#r7g(jkp(1#r%z-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR,'tayco/static']
+if DEBUG:
+     STATICFILES_DIRS=[BASE_DIR,'tayco/static']
+ else:
+     STATIC_ROOT= os.path.join(BASE_DIR, 'tayco/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
